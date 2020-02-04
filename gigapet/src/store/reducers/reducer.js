d5360b4909e6476d9d04623dc260d9c3f6fce23b
AislynnEdmiston <davidandaislynn@gmail.com>
2020-02-03 20:07:13 -0500

    added the sign up to the reducer

--- gigapet/src/store/reducers/reducer.js
+++ gigapet/src/store/reducers/reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/action';
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL, SIGNUP_START, SIGNUP_SUCCESS, SIGNUP_FAIL } from '../actions/action';
 
 const NO_ERRORS = null;
 
@@ -39,5 +39,32 @@ export const reducer = (state = Initial_State, action) => {
                 isAuthorizing: false,
                 error: action.payload
             };
+        case SIGNUP_START: 
+            return {
+                ...state,
+                user: {
+                    ...state.user
+                },
+                isAuthorizing: true,
+                error: NO_ERRORS
+            };
+        case SIGNUP_SUCCESS:
+            return {
+                ...state,
+                user: action.payload,
+                isAuthorizing: false,
+                error: NO_ERRORS
+            };
+        case SIGNUP_FAIL:
+            return {
+                ...state,
+                user: {
+                    ...state.user
+                },
+                isAuthorizing: false,
+                error: action.payload
+            };
+        default: 
+            return state;
     }
 }
