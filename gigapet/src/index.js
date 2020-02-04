0c335481703f8e7ba986be9dc03c1a1d5acd7cae
AislynnEdmiston <davidandaislynn@gmail.com>
2020-02-03 19:56:30 -0500

    created my store and set up middleware

--- gigapet/src/index.js
+++ gigapet/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// components
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux-store setup
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducer } from './store/reducers/reducer'
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
 
