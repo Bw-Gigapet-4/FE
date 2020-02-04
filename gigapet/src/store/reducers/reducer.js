fe9c61304e70860edc3b673ffa4ee5d4e971a3dd
AislynnEdmiston <davidandaislynn@gmail.com>
2020-02-03 19:56:11 -0500

    created my login reducers

---
+++ gigapet/src/store/reducers/reducer.js
@@ -0,0 +1,43 @@
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/action';
+
+const NO_ERRORS = null;
+
+export const Initial_State = {
+    user: {
+        id: -1,
+        username: ""
+    },
+    isAuthorizing: false,
+    error: NO_ERRORS
+}
+
+export const reducer = (state = Initial_State, action) => {
+    // console.log(action.type)
+    switch (action.type) {
+        case LOGIN_START: 
+            return { 
+                ...state,
+                user: {
+                    ...state.user
+                },
+                isAuthorizing: true,
+                error: NO_ERRORS
+            };
+        case LOGIN_SUCCESS: 
+            return {
+                ...state,
+                user: action.payload,
+                isAuthorizing: false,
+                error: NO_ERRORS
+            };
+        case LOGIN_FAIL:
+            return {
+                ...state,
+                user: {
+                    ...state.user
+                },
+                isAuthorizing: false,
+                error: action.payload
+            };
+    }
+}
